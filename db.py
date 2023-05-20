edcbf9c29df2753a28ef79980960e51f70e77737
TJ Bai <tjianbai@gmail.com>
2023-05-19 21:25:05 -0400

    stuff

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 WEST_TEAMS = ['DEN', 'MEM', 'SAC', 'PHO', 'LAC', 'GSW', 'LAL', 'MIN', 'NOP', 'OKC', 'DAL', 'UTA', 'POR', 'HOU', 'SAS']
 EAST_TEAMS = ['MIL', 'BOS', 'PHI', 'CLE', 'NYK', 'BRK', 'MIA', 'ATL', 'TOR', 'CHI', 'IND', 'WAS', 'ORL', 'CHO', 'DET']
+DBP = 'db/nba.db'
 
 class DB: 
     def __init__(self, db_file):
@@ -23,33 +24,48 @@ class DB:
         return self.cursor.fetchall()
 
     def create_tables(self):
-        self.cursor.execute('''
-            CREATE TABLE Game (
-                id INTEGER PRIMARY KEY,
-                good_guys TEXT,
-                bad_guys TEXT,
-                gg_points INTEGER,
-                bg_points INTEGER,
-                win BOOLEAN,
-                date TEXT
-            )
-        ''')
+        try:
+            self.cursor.execute('''
+                CREATE TABLE StartEnd (
+                    team TEXT,
+                    season INTEGER,
+                    start_date TEXT,
+                    end_date TEXT,
+                    PRIMARY KEY (team, season)  
+                )   
+            ''')
 
-        self.cursor.execute('''
-            CREATE TABLE Game_Official (
-                game_id INTEGER,
-                official_id INTEGER,
-                FOREIGN KEY (game_id) REFERENCES Game (id),
-                FOREIGN KEY (official_id) REFERENCES Official (id)
-            )
-        ''')
+            self.cursor.execute('''
+                CREATE TABLE Game (
+                    id INTEGER PRIMARY KEY,
+                    good_guys TEXT,
+                    bad_guys TEXT,
+                    gg_points INTEGER,
+                    bg_points INTEGER,
+                    win BOOLEAN,
+                    date TEXT
+                )
+            ''')
 
-        self.cursor.execute('''
-            CREATE TABLE Official (
-                id INTEGER PRIMARY KEY,
-                name TEXT UNIQUE
-            )
-        ''')
+            self.cursor.execute('''
+                CREATE TABLE Game_Official (
+                    game_id INTEGER,
+                    official_id INTEGER,
+                    FOREIGN KEY (game_id) REFERENCES Game (id),
+                    FOREIGN KEY (official_id) REFERENCES Official (id)
+                )
+            ''')
+
+            self.cursor.execute('''
+                CREATE TABLE Official (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT UNIQUE
+                )
+            ''')
+        
+        # dupe tables, we don't care
+        except sqlite3.OperationalError as e: 
+            print(e)
 
     """
     Each game should be a tuple of shape:
@@ -87,8 +103,6 @@ class DB:
         '''
 
         try:
-            self.conn.execute('BEGIN')
-
             for i, game in enumerate(games): 
                 # Insert game row
                 self.cursor.execute(game_query, game)
@@ -109,15 +123,35 @@ class DB:
             self.conn.rollback()
             print('Error inserting games: ', e)
 
+    def get_start_end(self, team: str, season: int) -> Tuple[int,int]:
+        query = 'SELECT start_date, end_date FROM StartEnd WHERE team = ? AND season = ?'
+        res = self.execute(query, params=(team, season))
+        return res[0]
+        
+    def get_official_from_id(self, id: str) -> str:
+        query = '''
+            SELECT name
+            FROM Official
+            WHERE id = ?
+        '''
+        params = (id,)
+        res = self.execute(query, params)
+        if len(res) == 0: return None
+        return res[0][0]
+
+    def get_game_from_id(self, id: str):
+        pass
+
     def test_insert_games(self):
-        games = [
-            ('LAL', 'GSW', 105, 100, True, '2022-05-13'),
-            ('MIL', 'BOS', 98, 102, False, '2022-05-12')
-        ]
-
+        games = [('LAL', 'GSW', 105, 100, True, '2022-05-13'), ('MIL', 'BOS', 98, 102, False, '2022-05-12')]
         officials = [['TJ Bai', 'Test'], ['TJ Bai', 'John Doe']]
-
         self.insert_games(games, officials)
+
+    def clear_all_tables(self):
+        input('Confirm deletion: ')
+        self.cursor.execute('DELETE FROM Game')
+        self.cursor.execute('DELETE FROM Game_Official')
+        self.cursor.execute('DELETE FROM Official')
 
 
 def init_table(): 
@@ -148,18 +182,36 @@ def check_pickles():
     sizes = {}
     for team, year in team_year_pairs(2012, 2022): 
         games, officials = grab_pickle(team, year)
-        if games is None or officials is None: raise Exception(f'Unable to parse pickle for {team} {year}')
+        if games is None or officials is None: 
+            raise Exception(f'Unable to parse pickle for {team} {year}')
 
         game_len, off_len = len(games), len(officials)
-        if game_len != off_len: raise Exception(f'{team} {year}: {game_len} != {off_len}')
+        if game_len != off_len: 
+            raise Exception(f'{team} {year}: {game_len} != {off_len}')
 
         if game_len not in sizes: sizes[game_len] = []
         sizes[game_len].append((team, year))
-    print(sizes.keys())
 
 def write_pickles_to_db():
-    for team, year in team_year_pairs(2012, 2022):
-	pass
+    with DB('db/nba.db') as db:
+        # clear previous state
+        db.clear_all_tables()
+
+        for team, year in team_year_pairs(2012, 2022):
+            games, officials = grab_pickle(team, year)
+            try: db.insert_games(games, officials)
+            except Exception as e:
+                print('Errored out at {team} {year}: ', e)
+                exit(1)
+            print(f'Just wrote {team} {year}')
+
+def populate_season_dates():
+    with DB('db/nba.db') as db:
+        for team, year in team_year_pairs(2012, 2022):  
+            games, _ = grab_pickle(team, year)
+            query = "INSERT INTO StartEnd (team, season, start_date, end_date) VALUES (?,?,?,?)"
+            params = (team, year, games[0][5], games[-1][5])
+            db.execute(query, params)
 
 if __name__ == '__main__': 
-    check_pickles()
+    populate_season_dates()
--- strat.py
+++ strat.py
@@ -1,7 +1,70 @@
+from typing import List, Tuple, Union
+
 from db import DB
 
-# def bet_on_winning_refs():
-#     pass
+DBP = 'db/nba.db'
 
-# if __name__ == '__main__':
-#     bet_on_winning_refs()
+def team_win_loss_with_ref(
+    team: str, 
+    off: Union[int, str], 
+    start: str = None, 
+    end: str = None,
+    season: Union[int, Tuple[int, int]] = None, # typescript is rubbing off
+):
+    with DB(DBP) as db:
+        if season is not None and isinstance(season, int):
+            start_date, end_date = db.get_start_end(team, season)
+        elif season is not None and isinstance(season, tuple):
+            start_date, _ = db.get_start_end(team, season[0])
+            _, end_date = db.get_start_end(team, season[1])
+        elif start is not None and end is not None:
+            start_date, end_date = start, end
+        else: raise TypeError()
+
+        if isinstance(off, int): 
+            oname = db.get_official_from_id(off)
+        elif isinstance(off, str): 
+            oname = off
+
+        query = '''
+            SELECT COUNT(*) as total_games,
+                SUM(CASE WHEN win THEN 1 ELSE 0 END) as total_wins
+            FROM Game
+            INNER JOIN Game_Official ON Game.id = Game_Official.game_id
+            INNER JOIN Official ON Game_Official.official_id = Official.id
+            WHERE date BETWEEN ? AND ?
+            AND (good_guys = ?)
+            AND Official.name = ?
+        '''
+        params = (start_date, end_date, team, oname)
+        res = db.execute(query, params)
+
+        if len(res) == 0: return None
+        total_games, total_wins = res[0]
+        return (total_wins, total_games - total_wins)
+
+def get_historic_officiating_team(good_guys: str, bad_guys: str, date: str) -> List[str]:
+    with DB(DBP) as db:
+        query = '''
+            SELECT O.name
+            FROM Game G
+            INNER JOIN Game_Official GO ON G.id = GO.game_id
+            INNER JOIN Official O ON GO.official_id = O.id
+            WHERE G.good_guys = ? AND G.bad_guys = ? AND G.date = ?
+        '''
+        params = (good_guys, bad_guys, date)
+        res = db.execute(query, params)
+
+        if len(res) == 0:
+            print("No matching game found")
+            exit(1) 
+        return [r[0] for r in res]
+
+def get_games():
+    pass
+
+if __name__ == '__main__':
+    # res = team_win_loss_with_ref('DEN', 1, season=('2012', '2014'))
+    # print(res)
+    res = get_historic_officiating_team('DEN', 'DAL', '2011-12-26')
+    print(res)
